9e4bf53b343e14464135346bb920d626309f46e8
Filip <92389794+w60846@users.noreply.github.com>
2023-01-18 22:16:46 +0100

    fullpage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule }  from './app-routing.module';
 import {DefaultModule} from "./layouts/default/default.module";
-
-
+import {FullpageModule} from "./layouts/fullpage/fullpage.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DefaultModule
+    DefaultModule,
+    FullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
